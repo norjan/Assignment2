97c3b1934b302bd743a22cfafa74ad041d158478
William Bowers <wbowers@coursera.org>
2014-04-27 14:28:26 -0700

    More comments

--- run_analysis.R
+++ run_analysis.R
@@ -93,4 +93,5 @@ run.analysis <- function(dir) {
   write.table(all.data.clean, clean.file.name, row.names = FALSE, quote = FALSE)
 }
 
+# Run the analysis.
 run.analysis("/repos/Getting-and-Cleaning-Data-Project/data")
